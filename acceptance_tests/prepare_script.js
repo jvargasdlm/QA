abca787b1151039c7c9338b6ce1bf42d4a4c3192
FlaviaAP <flavia.avila@ufrgs.br>
2019-09-27 14:42:27 -0300

    makefile init rule receiving login data args

--- acceptance_tests/prepare_script.js
+++ acceptance_tests/prepare_script.js
@@ -2,16 +2,30 @@ require('custom-env').env('staging');
 const FileHandler = require('./code/handlers/FileHandler');
 
 function setLoginData(){
+
+    let email = process.env.EMAIL;
+    if(email === 'empty'){
+        email = process.env.DEFAULT_EMAIL;
+    }
+
+    let password = process.env.PASSWORD;
+    if(password === 'empty'){
+        password = process.env.DEFAULT_PASSWORD;
+    }
+
+    console.log(email, password);//.
+
     const data = {
         "OTUS": {
-            "email": process.env.EMAIL,
-            "password": process.env.PASSWORD
+            "email": email,
+            "password": password
         },
         "OTUS_DOMAIN": {
             "email": "",
             "password": ""
         }
     };
+
     const content = JSON.stringify(data,null,4);
     FileHandler.write('./login_data.json', content);
     console.log('Login data saved successfully.');
