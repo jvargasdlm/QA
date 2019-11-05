c5a565797fde17d3db5043162031651c1b3ea894
FlaviaAP <flavia.avila@ufrgs.br>
2019-11-05 09:55:43 -0300

    start Card, ActivityCard classes

---
+++ acceptance_tests/code/otus/classes/ActivityCard.js
@@ -0,0 +1,15 @@
+const Card = require("../../classes/Card");
+
+class ActivityCard extends Card {
+
+    constructor(pageExt){
+        super(pageExt)
+    }
+
+    async init(){
+
+    }
+
+}
+
+module.exports = ActivityCard;
