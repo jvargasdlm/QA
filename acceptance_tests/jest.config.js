150c5dc8ccd3a329d55869265193451d3a210699
FlaviaAP <flavia.avila@ufrgs.br>
2019-12-11 10:55:37 -0300

    simplify jest config and call

--- acceptance_tests/jest.config.js
+++ acceptance_tests/jest.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  testMatch: [
-    // '**/tests/otus/activity-redesign-test.js',
-    '**/tests/otus/activity-adder-test.js'
-  ],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.(js|jsx)?$': 'babel-jest'
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  //snapshotSerializers: ['jest-serializer-vue'],
-  setupFiles: ['./jest.setup.js'],
-  setupFilesAfterEnv: ["jest-extended"]
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+    testMatch: [
+      // '**/tests/otus/activity-redesign-test.js',
+      '**/tests/otus/activity-adder-test.js'
+    ],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+      '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    setupFilesAfterEnv: ["jest-extended"]
 };
