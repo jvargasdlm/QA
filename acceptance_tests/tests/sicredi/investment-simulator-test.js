60809996666bb15cc3402eda739716ae4831fcd6
Javier Vargas De la Maza <javiervargasdlm@gmail.com>
2020-02-12 19:20:57 -0300

    revisão 18 testes

--- acceptance_tests/tests/sicredi/investment-simulator-test.js
+++ acceptance_tests/tests/sicredi/investment-simulator-test.js
@@ -94,137 +94,178 @@ describe('1. Test suite of Sicredi Investment Simulator: Profile for you',()=> {
         expect(componentInvest).toBe("Valor mínimo de 20.00");
     })
 
-    xtest('Test 1.4: Blank amount to be applied and amount to invest less than 20 reais', async ()=>{
+    test('Test 1.4: Blank amount to be applied and amount to invest less than 20 reais', async ()=>{
         await fillFormForYou("", "19,99", "20")
         const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-        expect(guidanceMessageAmountToApply).not.toBeNull();
         const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+        const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+        const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+        expect(guidanceMessageAmountToApply).not.toBeNull();
         expect(guidanceMessageAmountToInvest).not.toBeNull();
+        expect(componentApply).toBe("Valor mínimo de 20.00");
+        expect(componentInvest).toBe("Valor mínimo de 20.00");
     })
 
-    xtest('Test 1.5: Blank amount to be applied and amount to invest equal than 20 reais',async()=>{
+    test('Test 1.5: Blank amount to be applied and amount to invest equal than 20 reais',async()=>{
             await fillFormForYou("", "20,00", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
         }
     )
 
-    xtest('Test 1.6: Amount to apply less than 20 reais and amount to invest in blank',async()=>{
+    test('Test 1.6: Amount to apply less than 20 reais and amount to invest in blank',async()=>{
             await fillFormForYou("19,99", "", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
 
-    xtest('Test 1.7: Amount to apply less than 20 reais and amount to invest equal than 20 reais',async()=>{
+    test('Test 1.7: Amount to apply less than 20 reais and amount to invest equal than 20 reais',async()=>{
             await fillFormForYou("19,99", "20,00", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
         }
     )
 
-    xtest('Test 1.8: Amount to apply equal than 20 reais and amount to invest in blank',async()=>{
+    test('Test 1.8: Amount to apply equal than 20 reais and amount to invest in blank',async()=>{
             await fillFormForYou("20,00", "", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
 
-    xtest('Test 1.9: Amount to apply equal than 20 reais and amount to invest less than 20 reais',async()=>{
+    test('Test 1.9: Amount to apply equal than 20 reais and amount to invest less than 20 reais',async()=>{
             await fillFormForYou("20,00", "19,99", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
 });
 
-xdescribe('2. Suite teste of Sicredi Investment Simulator: Profile for company or agribusiness',()=> {
+describe('2. Suite teste of Sicredi Investment Simulator: Profile for company or agribusiness',()=> {
 
     test('Test 2.1: Amount to apply and amount to invest equal than 20 reais', async ()=>{
-        await fillFormForCompany("20,00", "20,00", "20")
+        await fillFormForCompany("20,00", "20,00", "20");
         const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-        expect(guidanceMessageAmountToApply).toBeNull();
         const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+        const componentResult = await extractComponent(selectors.DIV_RESULT);
+        expect(guidanceMessageAmountToApply).toBeNull();
         expect(guidanceMessageAmountToInvest).toBeNull();
+        expect(componentResult.includes("Veja estas outras opções para você")).toBeTrue();
     })
 
     test('Test 2.2: Amount to apply and amount to invest less than 20 reais', async ()=>{
         await fillFormForCompany("19,99", "19,99", "20")
         const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-        expect(guidanceMessageAmountToApply).not.toBeNull();
+        const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+        const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
         const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+        expect(guidanceMessageAmountToApply).not.toBeNull();
         expect(guidanceMessageAmountToInvest).not.toBeNull();
+        expect(componentApply).toBe("Valor mínimo de 20.00");
+        expect(componentInvest).toBe("Valor mínimo de 20.00");
     })
 
     test('Test 2.3: Blank values', async ()=>{
         await fillFormForCompany("", "", "20")
         const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-        expect(guidanceMessageAmountToApply).not.toBeNull();
         const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+        const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+        const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+        expect(guidanceMessageAmountToApply).not.toBeNull();
         expect(guidanceMessageAmountToInvest).not.toBeNull();
+        expect(componentApply).toBe("Valor mínimo de 20.00");
+        expect(componentInvest).toBe("Valor mínimo de 20.00");
     })
 
     test('Test 2.4: Blank amount to be applied and amount to invest less than 20 reais', async ()=>{
         await fillFormForCompany("", "19,99", "20")
         const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-        expect(guidanceMessageAmountToApply).not.toBeNull();
         const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+        const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+        const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+        expect(guidanceMessageAmountToApply).not.toBeNull();
         expect(guidanceMessageAmountToInvest).not.toBeNull();
+        expect(componentApply).toBe("Valor mínimo de 20.00");
+        expect(componentInvest).toBe("Valor mínimo de 20.00");
     })
 
     test('Test 2.5: Blank amount to be applied and amount to invest equal than 20 reais',async()=>{
             await fillFormForCompany("", "20,00", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
         }
     )
 
     test('Test 2.6: Amount to apply less than 20 reais and amount to invest in blank',async()=>{
             await fillFormForCompany("19,99", "", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
 
     test('Test 2.7: Amount to apply less than 20 reais and amount to invest equal than 20 reais',async()=>{
             await fillFormForCompany("19,99", "20,00", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).not.toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentApply = await extractComponent(selectors.LABEL_AMOUNT_TO_APPLY);
+            expect(guidanceMessageAmountToApply).not.toBeNull();
             expect(guidanceMessageAmountToInvest).toBeNull();
+            expect(componentApply).toBe("Valor mínimo de 20.00");
         }
     )
 
     test('Test 2.8: Amount to apply equal than 20 reais and amount to invest in blank',async()=>{
             await fillFormForCompany("20,00", "", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
 
     test('Test 2.9: Amount to apply equal than 20 reais and amount to invest less than 20 reais',async()=>{
             await fillFormForCompany("20,00", "19,99", "20");
             const guidanceMessageAmountToApply = await page.$(selectors.LABEL_AMOUNT_TO_APPLY);
-            expect(guidanceMessageAmountToApply).toBeNull();
             const guidanceMessageAmountToInvest = await page.$(selectors.LABEL_AMOUNT_TO_INVEST);
+            const componentInvest = await extractComponent(selectors.LABEL_AMOUNT_TO_INVEST);
+            expect(guidanceMessageAmountToApply).toBeNull();
             expect(guidanceMessageAmountToInvest).not.toBeNull();
+            expect(componentInvest).toBe("Valor mínimo de 20.00");
         }
     )
-
 });
